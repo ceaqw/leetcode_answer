5f94b7b4f555c684d59121921f5bb8b381fe4b29
ceaqw <ceaqw@egitee.com>
2021-01-01 12:52:08 +0800

    test2

---
+++ two-sum.py
@@ -0,0 +1,23 @@
+# coding: utf8
+# 题目地址：https://leetcode-cn.com/problems/two-sum/
+
+from typing import List
+
+
+class Solution:
+    count = 0
+    sum_index = []
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_1 = nums[0]
+        for num_index in range(1,len(nums)):
+            if num_1+nums[num_index] == target:
+                self.sum_index = [self.count, self.count+num_index]
+                return self.sum_index
+        self.count += 1
+        self.twoSum(nums[1:], target)
+        return self.sum_index
+
+
+a = Solution()
+
+print(a.twoSum([2,7,11,15], 9))
