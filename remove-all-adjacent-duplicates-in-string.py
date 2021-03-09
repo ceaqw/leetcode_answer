fd696d096b51545ebbb34eb3631c50625a784c43
ceaqw <2487689294@qq.com>
2021-03-09 16:59:58 +0800

    20210309

---
+++ remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,19 @@
+# coding: utf8
+# datetime: 2021-03-07
+# 题目地址: https://leetcode-cn.com/problems/remove-all-adjacent-duplicates-in-string/
+
+
+class Solution:
+    def removeDuplicates(self, S: str) -> str:
+        stack = []
+        for i in S:
+            if len(stack) > 0 and i == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(i)
+        return "".join(stack)
+
+
+a = Solution()
+print(a.removeDuplicates("abbaca"))
+        
