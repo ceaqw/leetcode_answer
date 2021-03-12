b0ce94e75f581b4eaff854260d15b6c612e5fc18
ceaqw <2487689294@qq.com>
2021-03-12 12:00:23 +0800

    20210212

---
+++ verify-preorder-serialization-of-a-binary-tree.py
@@ -0,0 +1,30 @@
+# coding: utf8
+# datetime: 2021-03-012
+# 题目地址: https://leetcode-cn.com/problems/verify-preorder-serialization-of-a-binary-tree/
+
+
+
+class Solution:
+    def isValidSerialization(self, preorder: str) -> bool:
+        stack = [1]
+        preorder_list = preorder.split(',')
+        for ch in preorder_list:
+            if not stack:
+                return False
+            if ch == '#':
+                stack[-1] -= 1
+                if stack[-1] == 0:
+                    stack.pop()
+            else:
+                stack[-1] -= 1
+                if stack[-1] == 0:
+                    stack.pop()
+                stack.append(2)
+            # print(stack)
+        if stack:
+            return False
+        return True
+
+
+a = Solution()
+print(a.isValidSerialization("9,3,4,#,#,1,#,#,2,#,6,#,#"))
